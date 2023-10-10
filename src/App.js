01cb26b8d3286470e441396729b0b9d1d0d5293f
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-10-11 00:05:00 +0300

    Add components elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import HomePage from './components/HomePage';
+import CountryDetails from './components/CountryDetails';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route path="/" element={' '} />
-        <Route path="/country" element={' '} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/country/:countryName" element={<CountryDetails />} />
       </Routes>
     </BrowserRouter>
   );
