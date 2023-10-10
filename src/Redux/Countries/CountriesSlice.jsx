7f739080f7bbb4201b019842c8364b867eb39cdc
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-10-11 00:05:53 +0300

    Create CountriesSlice.jsx

---
+++ src/Redux/Countries/CountriesSlice.jsx
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const apiLink = 'https://restcountries.com/v3.1/all';
+
+export const getCountriesData = createAsyncThunk('countries/getCountries', async (thunkAPI) => {
+  try {
+    const response = await axios.get(apiLink);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+let countriesState = {};
+export const countriesSlice = createSlice({
+  name: 'countries',
+  initialState: {
+    countries: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCountriesData.pending, (state) => {
+        countriesState = { ...state, isLoading: false };
+        return countriesState;
+      })
+      .addCase(getCountriesData.fulfilled, (state, action) => {
+        countriesState = { ...state, isLoading: false };
+        const countries = action.payload.map((country) => ({
+          commonName: country.name.common,
+          cca2: country.cca2,
+          region: country.region,
+          flag: country.flags,
+          coatOfArms: country.coatOfArms,
+          map: country.maps,
+        }));
+        countriesState = { ...state, isLoading: true, countries };
+        return countriesState;
+      })
+      .addCase(getCountriesData.rejected, (state, action) => {
+        countriesState = { ...state, isLoading: false, error: action.error };
+        return countriesState;
+      });
+  },
+});
+
+export default countriesSlice.reducer;
